aebdac32bd7fba26321563d328233e568de35b22
Manuel Kao <manuelsko@live.de>
2023-02-21 16:25:22 +0100

    add success and bug message

--- components/form.js
+++ components/form.js
@@ -1,5 +1,7 @@
 import { useReducer } from "react";
 import { BiPlus } from "react-icons/bi";
+import Success from "./success";
+import Bug from "./bug";
 
 const formReducer = (state, event) => {
   return {
@@ -13,8 +15,13 @@ export default function Form() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (Object.keys(formData).length == 0)
+      return console.log("Don't have Form Data");
     console.log(formData);
   };
+
+  if (Object.keys(formData).length > 0)
+    return <Success message={"Data Added"}></Success>;
 
   return (
     <form className="grid lg:grid-cols-2 w-4/6 gap-4" onSubmit={handleSubmit}>
