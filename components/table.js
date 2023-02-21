7002fb4bb7df35fd29a015e9e2f2218e68dd704c
Manuel Kao <manuelsko@live.de>
2023-02-21 17:34:06 +0100

    map data.json

--- components/table.js
+++ components/table.js
@@ -1,4 +1,5 @@
 import { BiEdit, BiTrashAlt } from "react-icons/bi";
+import data from "../database/data.json";
 
 export default function Table() {
   return (
@@ -26,37 +27,47 @@ export default function Table() {
         </tr>
       </thead>
       <tbody className="bg-gray-200">
-        <tr className="bg-gray-50 text-center">
-          <td className="px-16 py-2 flex flex-row items-center">
-            <img src="#" alt="" />
-            <span className="text-center ml-2 font-semibold">Manu Kao</span>
-          </td>
-          <td className="px-16 py-2 f">
-            <span className="">manukao@gmail.com</span>
-          </td>
-          <td className="px-16 py-2 f">
-            <span className="">Web Dev</span>
-          </td>
-          <td className="px-16 py-2 f">
-            <span className="">10-05-2022</span>
-          </td>
-          <td className="px-16 py-2 f">
-            <button className="cursor">
-              <span className="bg-green-500 text-white px-5 py-1 rounded-full">
-                Active
-              </span>
-            </button>
-          </td>
-          <td className="px-16 py-2 flex justify-around gap-5">
-            <button className="cursor">
-              <BiEdit size={25} color={"rgb(34,197,94)"}></BiEdit>
-            </button>
-            <button className="cursor">
-              <BiTrashAlt size={25} color={"rgb(244,63,94)"}></BiTrashAlt>
-            </button>
-          </td>
-        </tr>
+        {data.map((obj, i) => (
+          <Tr {...obj} key={i} />
+        ))}
       </tbody>
     </table>
+  );
+}
+
+function Tr({ id, name, avatar, email, bootcamp, start, status }) {
+  return (
+    <tr className="bg-gray-50 text-center">
+      <td className="px-16 py-2 flex flex-row items-center">
+        <img src={avatar || "#"} alt="user" />
+        <span className="text-center ml-2 font-semibold">
+          {name || "Unknown"}
+        </span>
+      </td>
+      <td className="px-16 py-2 f">
+        <span className="">{email || "Unknown"}</span>
+      </td>
+      <td className="px-16 py-2 f">
+        <span className="">{bootcamp || "Unknown"}</span>
+      </td>
+      <td className="px-16 py-2 f">
+        <span className="">{start || "Unknown"}</span>
+      </td>
+      <td className="px-16 py-2 f">
+        <button className="cursor">
+          <span className="bg-green-500 text-white px-5 py-1 rounded-full">
+            {status || "Unknown"}
+          </span>
+        </button>
+      </td>
+      <td className="px-16 py-2 flex justify-around gap-5">
+        <button className="cursor">
+          <BiEdit size={25} color={"rgb(34,197,94)"}></BiEdit>
+        </button>
+        <button className="cursor">
+          <BiTrashAlt size={25} color={"rgb(244,63,94)"}></BiTrashAlt>
+        </button>
+      </td>
+    </tr>
   );
 }
