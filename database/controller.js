10f0d4961a93a689c8760646c07643a7ab250b37
Manuel Kao <manuelsko@live.de>
2023-02-22 07:50:18 +0100

    create get and post request

--- database/controller.js
+++ database/controller.js
@@ -1,6 +1,7 @@
 /** Controller */
 import Users from "@/model/user";
 
+// get: http://localhost:3001/api/users
 export async function getUsers(req, res) {
   try {
     const users = await Users.find({});
@@ -8,5 +9,19 @@ export async function getUsers(req, res) {
     res.status(200).json(users);
   } catch (error) {
     res.status(404).json({ error: "Error While Fetching Data" });
+  }
+}
+
+// post: http://localhost:3001/api/users
+export async function postUsers(req, res) {
+  try {
+    const formData = req.body;
+    if (!formData)
+      return res.status(404).json({ error: "Form Data Not Provided...!" });
+    Users.create(formData, function (err, data) {
+      return res.status(200).json(data);
+    });
+  } catch (error) {
+    return res.status(404).json({ error });
   }
 }
